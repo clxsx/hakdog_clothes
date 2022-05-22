147307847c6c4a786b50b8200fd7d604624a8577
Collective <86518186+DevZiee@users.noreply.github.com>
2022-05-22 17:14:20 +0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,26 +2,8 @@ fx_version 'cerulean'
 games {'gta5'}
 lua54 'yes'
 author 'All credits to RKL Gaming'
-
-description 'Recode By Collective Original Script https://github.com/rklgaming/rkl_clothes'
-
-shared_scripts{
-	'@ox_lib/init.lua',
-	'@es_extended/imports.lua',
-	'config.lua',
-	'utils/utils.lua'
-} 
-
-client_scripts {
-	'client/main.lua'
-}
-
-server_scripts {
-	'server/main.lua'
-}
-
-dependency{
-	'es_extended',
-	'ox_inventory',
-	'ox_lib'
-}
+description 'This is a modified version of rkl_clothes by RKLGaming'
+shared_scripts{'@ox_lib/init.lua','@es_extended/imports.lua','config.lua','utils/utils.lua'} 
+client_scripts {'client/main.lua'}
+server_scripts {'server/main.lua'}
+dependency{'es_extended','ox_inventory','ox_lib'}
