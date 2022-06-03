1f2ec5f97a0dce79d575dfc25777320d80af44ad
Collective <86518186+DevZiee@users.noreply.github.com>
2022-06-04 04:09:19 +0800

    Update utils.lua

--- utils/utils.lua
+++ utils/utils.lua
@@ -1,11 +1,13 @@
-function PlayClothesAnims(dict, anim, wait)
-    loadAnimDict(dict)
+Collective = {}
+
+function Collective.PlayAnim(dict, anim, wait)
+    Collective.LoadAnimDict(dict)
     TaskPlayAnim(PlayerPedId(), dict, anim, 8.0, 1.0, -1, 49, 0, 0, 0, 0 )
-    Wait (wait)
+    Citizen.Wait(wait)
     ClearPedSecondaryTask(PlayerPedId()) 
 end
 
-function loadAnimDict(dict)
+function Collective.LoadAnimDict(dict)
     while (not HasAnimDictLoaded(dict)) do
         RequestAnimDict(dict)
         Citizen.Wait(5)
