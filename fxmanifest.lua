baf0ff4033aaae00299819f7e22bbc30131bdb29
Collective <86518186+DevZiee@users.noreply.github.com>
2022-08-14 19:40:11 +0800

    tweak: Removed

--- fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-games {'gta5'}
-lua54 'yes'
-author 'All credits to RKL Gaming'
-description 'This is a modified version of rkl_clothes by RKLGaming'
-shared_scripts{'@ox_lib/init.lua','@es_extended/imports.lua','config.lua','utils/utils.lua'} 
-client_scripts {'client/main.lua'}
-server_scripts {'server/main.lua'}
-dependency{'es_extended','ox_inventory','ox_lib'}
